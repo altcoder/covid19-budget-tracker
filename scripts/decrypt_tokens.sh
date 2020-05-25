ef2cbc67165fe6532cdaec0079515179a6b551f6
James Faeldon <111378+altcoder@users.noreply.github.com>
2020-05-25 13:11:42 +0800

    Fix for decrypt path

--- scripts/decrypt_tokens.sh
+++ scripts/decrypt_tokens.sh
@@ -3,4 +3,4 @@
 # Decrypt the file
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$TOKEN_PASSPHRASE" --output $HOME/credentials/g_oauth_clt.pickle $HOME/config/tokens/g_oauth_clt.pickle.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$TOKEN_PASSPHRASE" --output $GITHUB_WORKSPACE/credentials/g_oauth_clt.pickle $GITHUB_WORKSPACE/config/tokens/g_oauth_clt.pickle.gpg
